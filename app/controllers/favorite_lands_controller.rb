72b017cb8fc1065f1bf5b640b0b9d3b4d4fe7578
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-08-09 20:24:09 +0100

    land added

--- app/controllers/favorite_lands_controller.rb
+++ app/controllers/favorite_lands_controller.rb
@@ -10,9 +10,12 @@ class FavoriteLandsController < ApplicationController
   # POST /favorite_lands or /favorite_lands.json
   def create
     @favorite_land = FavoriteLand.new(favorite_land_params)
-
+    favorite = FavoriteLand.new 
+    favorite.land_id = @favorite_land[:land_id]
     respond_to do |format|
-      if @favorite_land.save
+      if !FavoriteLand.exist?(favorite)
+        format.html { redirect_to lands_url, notice: "This land is already created successfully." }
+      elsif @favorite_land.save
         format.html { redirect_to lands_url, notice: "Favorite land was successfully created." }
         format.json { render :show, status: :created, location: @favorite_land }
       else
--- app/models/favorite_land.rb
+++ app/models/favorite_land.rb
@@ -2,6 +2,7 @@ class FavoriteLand < ApplicationRecord
   belongs_to :profile
   belongs_to :land
   validates_presence_of :profile_id, :land_id
+  
 
 
   def self.find_all_by_user(user)
@@ -9,4 +10,14 @@ class FavoriteLand < ApplicationRecord
     favorite_land = FavoriteLand.includes(:land, :profile).where(profile_id: profile.id)
     favorite_land
   end
+
+  def self.find_by_land(profile_land)
+    FavoriteLand.includes(:land, :profile).find_by(land_id: profile_land.land_id)
+  end
+
+  def self.exist?(profile_land)
+    profile_land = FavoriteLand.find_by_land(profile_land)
+    profile_land.nil?
+  end
+
 end
