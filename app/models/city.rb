3ff262e73168ca0173a649388d8cefa0d9e753c4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-08-10 21:06:37 +0100

    validation added

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,6 @@
 class City < ApplicationRecord
   belongs_to :province
   has_many :address
+
+  validates_presence_of :province, :city_name
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,32 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  describe "City" do 
+
+  describe "model" do 
     let(:city) { create(:city) }
 
-
-    it { should validate_presence_of(:province) }
-
     it 'has a city' do 
-       
+       expect(city.nil?).to eq(false)
     end
 
     it 'has a province' do  
+      expect(city.province.province_name).to eq("Luanda")
     end
 
   end
 
   describe "validation" do 
+    let(:province) { create(:province) }
 
     before :context do 
-      @address = Address.new 
+      @city = City.new 
+    end
+
+    it 'has invalide city' do 
+      expect { @city.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'has invalide city_name' do 
+      @city.city_name = nil
+      @province = Province.new 
+      @province.province_name = "Luanda"
+      @city.province = @province
+
+      expect { @city.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it 'has invalide province' do 
+    it 'has invalide province' do
+      @city.province = nil
+      @city.city_name = "Kilamba"
+
+      expect { @city.save! }.to raise_error(ActiveRecord::RecordInvalid) 
     end
+
+    it { should validate_presence_of(:province) }
+    it { should validate_presence_of(:city_name)}
 
   end
+
+  describe "association" do 
+    it { should belong_to(:province)}
+    it { should have_many(:address)}
+  end
+
 end
