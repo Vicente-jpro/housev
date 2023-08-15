2b73b4d3b484695c9c4b67bb7040f0f25808e35f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-08-15 18:18:10 +0100

    form builded

--- app/models/concerns/enums_concerns.rb
+++ app/models/concerns/enums_concerns.rb
@@ -1,24 +1,31 @@
 module EnumsConcerns
     extend ActiveSupport::Concern
-    enum condition: { Used: 'USED', Renovated: 'RENOVATED', Unfinished:'UNFINISHED'}
-    enum type_negotiation: {
-        Rent_Monthly: 'RENT_MONTHLY', Rent_daily: 'RENT_DAILY', Rent_Shared: 'rent shared'
-     }
+    included do 
+        enum condition: { New: 'New', Renovated: 'RENOVATED', Unfinished:'UNFINISHED', Used: 'USED'}
+        enum type_negotiation: {
+            Rent_Monthly: 'RENT_MONTHLY', Rent_daily: 'RENT_DAILY', Rent_Shared: 'rent shared'
+        }
 
-    enum tipology: { T1:'T1', T2:'T2', T3:'T3', T4:'T4', T5:'T5' T6:'T6', T7:'T7', Others: 'OTHERS' }
+        enum tipology: { 
+            T1:'T1', T2:'T2', T3:'T3', T4:'T4', 
+            T5:'T5', T6:'T6', T7:'T7', Tn: 'OTHERS' 
+        }
 
-    enum next_by: { 
-        Church: 'CHURCH', Supermarket: 'SUPERMARKET', Food_court: 'FOOD_COURT',
-        Trade_square: 'TRADE_SQUARE', University: 'UNIVERSITY', 
-        Primary_school: 'PRIMARY_SCHOOL',Secondary_school: 'SECUNDARY_SCHOOL', 
-        High_school: 'HIGH_SCHOOL', Playground: 'PLAYGROUND', Leisure_park: 'LEISURE_PARK', 
-        Gymnasium: 'GYMANASIUM', Football_stadium:'FOOTBALL_STADIUM', Main_Road: 'MAIN_ROAD', 
-        Train_or_metrol_station: 'TRAIN_OR_METROL_STATION', Airport:'AIRPORT', 
-        Militar_station: 'MILITAR_STATION', Police_office: 'POLICE_OFFICE', Others: 'OTHERS'
-    }
+        enum next_by: { 
+            Airport:'AIRPORT', 
+            Church: 'CHURCH', Food_court: 'FOOD_COURT', Football_stadium:'FOOTBALL_STADIUM', 
+            Leisure_park: 'LEISURE_PARK', Militar_station: 'MILITAR_STATION', 
+            Main_Road: 'MAIN_ROAD', High_school: 'HIGH_SCHOOL', Gymnasium: 'GYMANASIUM',
+            Police_office: 'POLICE_OFFICE', Playground: 'PLAYGROUND',
+            Primary_school: 'PRIMARY_SCHOOL', Supermarket: 'SUPERMARKET', 
+            Secondary_school: 'SECUNDARY_SCHOOL', Trade_square: 'TRADE_SQUARE', 
+            Train_or_metrol_station: 'TRAIN_OR_METROL_STATION', University: 'UNIVERSITY'
+        }
 
-    enum property_type: { Country_house: 'COUNTRY_HOUSE', Apartamento: 'APARTAMENTO', 
-        Store:'STORE', Office: 'OFFICE', Enterprise: 'ENTERPRISE', Hotel: 'HOTEL', Room: 'ROOM',
-        Shop_store: 'SHOP_STORE', Restaurant: 'RESTAURANT', Farm: 'FARM', Others: 'OTHERS'
-    }
+        enum property_type: { Apartamento: 'APARTAMENTO', Country_house: 'COUNTRY_HOUSE', 
+            Enterprise: 'ENTERPRISE', Hotel: 'HOTEL',  Farm: 'FARM',  Office: 'OFFICE',
+            Store:'STORE', Room: 'ROOM', Shop_store: 'SHOP_STORE', 
+            Restaurant: 'RESTAURANT', Others: 'OTHERS'
+        }
+    end
 end
