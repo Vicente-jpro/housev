1da1cbae904ec1e69b3263b2d002ab11ee232321
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-10-01 17:11:24 +0100

    Location switched added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
     add_flash_types :info
+    around_action :switch_locale
+
+    def switch_locale(&action)
+    locale = ""
+    if params[:locale] == "pt"
+        locale = "pt"
+    elsif params[:locale] == "en"
+        locale = "en"
+    elsif locale == ""
+        locale = params[:locale] || I18n.default_locale
+    end
+    debugger
+    I18n.with_locale(locale, &action)
+    end
+
+    
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  around_action :switch_locale
+
   def index
   end
+
+  def switch_locale(&action)
+  locale = ""
+  if params[:locale] == "pt"
+      locale = "pt"
+  elsif params[:locale] == "en"
+      locale = "en"
+  elsif locale == ""
+      locale = params[:locale] || I18n.default_locale
+  end
+  I18n.with_locale(locale, &action)
+  end
+
 end
