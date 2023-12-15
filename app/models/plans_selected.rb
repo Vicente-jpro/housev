0fcd0557e8e2888ccbf2aa2bb5655ec6102d0068
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-12-15 21:22:11 +0100

    How plan to update

--- app/models/plans_selected.rb
+++ app/models/plans_selected.rb
@@ -3,5 +3,11 @@ class PlansSelected < ApplicationRecord
   belongs_to :user
 
   enum duration: { monthly: 30, quarterly: 90, semiannual: 180,  annual: 360 }
+
+  validates :user, uniqueness: {  message: "User can have only one plan selected." }
+
+
+  scope :find_by_user, ->(user) { where(user_id: user.id).take }
+
        
 end
