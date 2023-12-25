8e7d96152e546073bda11fec4a4fb86bf040ae98
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-12-26 00:03:20 +0100

    Added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
   $(".pais").click(function(){
     alert("pais")
   })
+
+
+ $.get(
+  "https://jsonplaceholder.typicode.com/users", 
+  function(data, status){
+  console.log(data, status);
+});
+
 });
