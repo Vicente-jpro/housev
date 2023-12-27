7ce73f50b468712206c4b7030e7c22bf54da04aa
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-12-27 16:34:11 +0100

    Dependent select added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,21 +1,29 @@
 
 $(document).ready(function(){
 
+  // Fist select
   $(".province").click(function(){
     var province_id = $('#profile_province_id').val()
     console.log('Province id: ', province_id)
 
+    // Second select
     $.get(
-      `http://localhost:3000/cities/${province_id}/provinces`, 
+      `http://localhost:3000/cities/${province_id}/province`, 
       function(data, status){
       console.log(data, status);
       
+      $('#profile_address_attributes_city_id')
+      .empty()
+
+     for (var index = 0; index <= data.length; index++) {
+        $('#profile_address_attributes_city_id')
+          .append(`<option value="${data[index].id}"> ${data[index].city_name} </option>`);
+     }
+
     });
 
-    
-    alert("province")
+
+
   })
 
-  var name = $('name #profile_address_attributes_city_id').text()
-  console.log('name: ', name)
 });
