3fca9ed0e5616c70cd4e34df7288ceca298fc5ed
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-01-03 21:43:28 +0100

    Success message added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -42,7 +42,7 @@ class ContactsController < ApplicationController
                 .deliver_later
         end
 
-        format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
+        format.html { redirect_to contact_url(@contact), notice: "Message sent successfully." }
         format.json { render :show, status: :created, location: @contact }
       else
         format.html { render :new, status: :unprocessable_entity }
