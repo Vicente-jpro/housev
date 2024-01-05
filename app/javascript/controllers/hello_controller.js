5144377e9134fb857abb7c6ee8798bf5f87fc641
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-01-05 17:14:51 +0100

    controller locale removed

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello Worldadsads!"
-   
+    this.element.textContent = "Hello World!"
   }
 }
